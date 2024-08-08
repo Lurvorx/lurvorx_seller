bcd645b5f1849fc356fdc6986bbe60647d9bfafa
Lurvorx <88720533+Lurvorx@users.noreply.github.com>
2024-08-09 00:22:22 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,18 @@
+----------------------------------------------------
+--                                                --
+--                 Lurvorx Scripts                --
+--      Basic FiveM scripts made with quality     --
+--           https://github.com/Lurvorx           --
+--          https://discord.gg/jJnE7yTYZW         --
+--                                                --
+----------------------------------------------------
+
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
 author "Lurvorx Scripts"
-description "A basic item seller script made by Lurvorx Scripts."
+description "A simple and basic item seller script made by Lurvorx Scripts"
 
 client_script {
     "client/client.lua"
