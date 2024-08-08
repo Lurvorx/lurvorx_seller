6cbac1890035313ec8cffb3259a6844ea21a71d1
Lurvorx <88720533+Lurvorx@users.noreply.github.com>
2024-08-09 00:21:31 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,12 @@
-ESX = nil
 ESX = exports["es_extended"]:getSharedObject()
 
 SendToDiscord = function(playerName, discordMessage)
     local embeds = {
         {
             ['type'] = 'rich',
-            ['title'] = 'ITEM SELLER LOGS',
+            ['title'] = '`💵` ITEM SELLER LOGS',
             ['description'] = discordMessage,
-            ['color'] = 10092339,
+            ['color'] = 3447003,
             ['footer'] = {
                 ['text'] = 'Lurvorx Scripts Logs | ' .. os.date(),
                 ['icon_url'] = 'https://cdn.discordapp.com/attachments/1185300625320329296/1185630929847337000/Lurvorx-Scripts-Logga.jpg?ex=6675b4d9&is=66746359&hm=154e4c80ac278286f9b24f7e1b832fe2afcddad8d4801c62e7656d4b1a79da55&'
@@ -15,11 +14,11 @@ SendToDiscord = function(playerName, discordMessage)
         }
     }
 
-    PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({ embeds = embeds}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({embeds = embeds}), { ['Content-Type'] = 'application/json' })
 end
 
 RegisterServerEvent("lurvorx_seller:sellItem")
-AddEventHandler("lurvorx_seller:sellItem", function(item, amount, money)
+AddEventHandler("lurvorx_seller:sellItem", function(item, amount, price)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     local playerName = GetPlayerName(_source)
@@ -27,20 +26,35 @@ AddEventHandler("lurvorx_seller:sellItem", function(item, amount, money)
 
     if itemCount and itemCount.count > 0 then
         xPlayer.removeInventoryItem(item, amount)
-        local amountGettingMoney = xPlayer.addMoney(money)
-        TriggerClientEvent("esx:showNotification", source, Config.Locale.Notification.soldItem .. " " .. amount .. " ~g~" .. item, "success", Config.NotificationTime)
+        xPlayer.addMoney(price)
+        TriggerClientEvent("esx:showNotification", source, Config.Strings.Notification.soldItem .. " " .. amount .. Config.Strings.Notification.soldItemColor .. " " .. item, "success", Config.NotificationTime * 1000)
     else
-        TriggerClientEvent("esx:showNotification", source, Config.Locale.Notification.noItem .. " ~r~" .. item, "error", Config.NotificationTime)
+        TriggerClientEvent("esx:showNotification", source, Config.Strings.Notification.noItem .. item, "error", Config.NotificationTime * 1000)
         return
     end
 
-    local discordid = string.gsub(GetPlayerIdentifier(_source, 1), "discord:", "") or "N/A"
-    local fivem = GetPlayerIdentifier(_source, 2) or "N/A"
-    local license = GetPlayerIdentifier(_source) or "N/A"
-    local license2 = GetPlayerIdentifier(_source, 3) or "N/A"
+    for k,v in pairs(GetPlayerIdentifiers(source)) do
+        if string.sub(v, 1, string.len("steam:")) == "steam:" then
+            steamid = v
+        elseif string.sub(v, 1, string.len("license:")) == "license:" then
+            license = v
+        elseif string.sub(v, 1, string.len("license2:")) == "license2:" then
+            license2 = v
+        elseif string.sub(v, 1, string.len("fivem:")) == "fivem:" then
+            fivem = v
+        elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
+            discord = v
+        end
+    end
+
+    steamhex = steamid or "N/A"
+    fivemlicense = license or "N/A"
+    fivemlicense2 = license2 or "N/A"
+    fivemid = fivem or "N/A"
+    discordid = string.gsub(discord, "discord:", "") or "N/A"
 
     SendToDiscord(
-        discordMessage, "**" .. playerName .. "** have sold **" .. amount .. "** item(s)." .. "\n\n`📃` **ITEM NAME:** `" .. item .. "`\n`📃` **ITEMS LEFT:** `" .. itemCount.count - amount .. "`\n\n`👤` **PLAYER:** `" .. playerName .. "`\n`🔢` **SERVER ID:** `" .. _source .. "`\n`💬` **DISCORD:** " .. "<@" .. discordid .. "> [||" .. discordid .. "||]" .. "\n`🎮` **FIVEM:** ||" .. fivem .. "||\n`💿` **LICENSE:** ||" .. license .. "||\n`📀` **LICENSE 2:** ||" .. license2 .. "||"
+        discordMessage, "**" .. playerName .. "** have sold **" .. amount .. "** item(s)." .. "\n\n`📃` **ITEM NAME:** `" .. item .. "`\n`📃` **ITEMS LEFT:** `" .. itemCount.count - amount .. "`\n\n`👤` **PLAYER:** `" .. playerName .. "`\n`🔢` **SERVER ID:** `" .. source .. "`\n`💬` **DISCORD:** " .. "<@" .. discordid .. "> [||" .. discordid .. "||]" .. "\n`🎮` **STEAM HEX:** ||" .. steamhex .. "||\n`🎮` **FIVEM:** ||" .. fivemid .. "||\n`💿` **LICENSE:** ||" .. fivemlicense .. "||\n`📀` **LICENSE 2:** ||" .. fivemlicense2 .. "||"
     )
 
 end)
